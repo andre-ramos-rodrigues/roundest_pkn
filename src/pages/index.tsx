8a468a22fdfaba4efe29d04cac54e769d2e3e326
Andre <andreramos@id.uff.br>
2022-08-31 15:25:04 -0300

    UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
 
 const Home: NextPage = () => {
   return (
-   <div className='text-2xl text-red-200'>Hello</div>
+   <div className='h-screen w-screen flex flex-col justify-center items-center'>
+    <div className='text-2xl text-center'>Which Pokémon would win in a battle?</div>
+    <div className='p-6' />
+    <div className='border rounded p-8 flex justify-between items-center content-center max-w-2xl flex-col md:flex-row animate-fade-in'>
+      <div className='w-16 h-16 bg-red-200'>Pikachu</div>
+      <div className='p-8'>vs</div>
+      <div className='w-16 h-16 bg-red-200'>Rayquaza</div>
+    </div>
+   </div>
   );
 };
 
