122d2d371b8bd1b94d68d8e691f423f680655528
Andre <andreramos@id.uff.br>
2022-08-31 15:03:07 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+type TechnologyCardProps = {
+  name: string;
+  description: string;
+  documentation: string;
+};
+
+const Home: NextPage = () => {
+  return (
+   <div className='text-2xl text-red-200'>Hello</div>
+  );
+};
+
+
+
+export default Home;
